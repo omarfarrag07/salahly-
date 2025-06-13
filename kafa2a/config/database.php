de7a4904078772c29047ba7132beddef2bbad014
EngySherif <engysherif123@gmail.com>
2025-06-13 06:51:23 +0300

    Update database.php

--- kafa2a/config/database.php
+++ kafa2a/config/database.php
@@ -86,7 +86,7 @@ return [
             'driver' => 'pgsql',
             'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
+            'port' => env('DB_PORT', '5050'),
             'database' => env('DB_DATABASE', 'forge'),  
             'username' => env('DB_USERNAME', 'forge'),
             'database' => env('DB_DATABASE', 'laravel'),
